1094bb6762cb01ba8c152c603e21d81b860efcb0
srideep-banerjee <banerjee.srideep@gmail.com>
2023-11-27 11:46:18 +0530

    Day 1 Part 2 complete

--- Day1/part2.go
+++ Day1/part2.go
@@ -4,38 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"unicode/utf8"
 )
 
-func PrintAns1(file *os.File) {
+func PrintAns2(file *os.File) {
 	var sc = bufio.NewScanner(file)
-
-	maxSum := 0
+	
+	sums := []int{0}
 	curSum := 0
+	
 	for sc.Scan() {
 		var line string = sc.Text()
 		
 		if utf8.RuneCountInString(line) == 0 {
+			sums = append(sums, curSum)
 			curSum = 0
 		} else {
 			num, err := strconv.Atoi(line)
 			HandleError(err)
 			curSum += num
-			if curSum > maxSum {
-				maxSum = curSum
-			}
 		}
 	}
+	if curSum != 0 {
+		sums = append(sums, curSum)
+	}
 
+	sort.Slice(sums, func(i, j int) bool {
+		return sums[i]>sums[j]
+	})
+		
 	HandleError(sc.Err())
 
-	fmt.Println("Day 1 Part 1 ans is", maxSum)
-}
-
-func HandleError(err error) {
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+	fmt.Println("Day 1 Part 2 ans is", sums[0] + sums[1] + sums[2])
 }
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		panic(err)
 	}
 	
-	day1.PrintAns1(file)
+	day1.PrintAns2(file)
 
 	err = file.Close()
 	if(err != nil) {
