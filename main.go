aaade1307ef71acd468f384c05f3187b692b26ac
srideep-banerjee <banerjee.srideep@gmail.com>
2023-12-04 17:08:50 +0530

    2023 Day 4 part 2 completed

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"example/2023/day3"
+	"example/2023/day4"
 	"os"
 )
 
@@ -11,7 +11,7 @@ func main() {
 		panic(err)
 	}
 	
-	day3.PrintAns2(file)
+	day4.PrintAns1(file)
 
 	err = file.Close()
 	if(err != nil) {
